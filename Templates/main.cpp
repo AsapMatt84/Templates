b426e1347852980125230286b37ac067916baf0b
AsapMatt84 <matthew.cahill@sinclair.edu>
2021-07-30 15:07:55 -0400

    Initial Commit

---
+++ Templates/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Templates
+//
+//  Created by Matt Cahill on 7/30/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
